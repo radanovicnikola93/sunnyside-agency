7b693ebf6da23a97b1771d8a74ce4141210f3cb8
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-25 18:19:42 +0100

    changed animation on the hamburger button

--- app.js
+++ app.js
@@ -16,5 +16,6 @@ const navLinks = document.querySelector('.nav--links');
 
 
 hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
     navLinks.classList.toggle('active');
 })
